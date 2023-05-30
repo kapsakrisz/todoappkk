44ee6d8669798b2b45bda5a146023bd5383f3ffd
devkapsakrisz <devkapsakrisz@gmnail.com>
2023-05-30 14:45:21 +0100

    working on header2

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -23,7 +23,7 @@ className='w-44 md:w-56 pb-1 object-contain'
 
 />
 
-<div className='flex '>
+<div className='flex items-center space-x-5 flex-1 justify-end w-full'>
     <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
         <MagnifyingGlassIcon className='h-6 w-6 text-gray-400'/>
         <input className='flex-1 outline-none p-2' type='text' placeholder='search'/>
