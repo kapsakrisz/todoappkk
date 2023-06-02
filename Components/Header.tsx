8cde46dc68b6813037fd14cf009118b48ac64575
devkapsakrisz <devkapsakrisz@gmnail.com>
2023-06-02 17:37:08 +0100

    wohoo

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -2,13 +2,39 @@
 
 
 
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import Image from 'next/image';
 import {MagnifyingGlassIcon,UserCircleIcon} from "@heroicons/react/24/solid";
 import Avatar from 'react-avatar';
+import { useBoardStore } from '@/store/BoardStore';
+import fetchSuggestion from '@/lib/fetchSuggestion';
 
 function Header () {
-    return (
+
+        const [board,searchString,setSearchString]= useBoardStore((state)=>[
+            
+            state.board,
+            state.searchString,
+            state.setSearchString,
+        ]);
+        const [loading,setLoading]=useState<boolean>(false);
+        const [suggestion, setSuggestion]=useState<string>("");
+
+        useEffect(()=> {
+            if (board.columns.size===0) return;
+            setLoading(true);
+
+            const fetchSuggestionFunc = async ()=> {
+                const suggestion = await fetchSuggestion(board);
+                setSuggestion(suggestion);
+                setLoading(false);
+            };
+            fetchSuggestionFunc();
+        },[board]);
+
+    
+    
+        return (
 
 <header>
 <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10'>
@@ -27,7 +53,14 @@ className='w-44 md:w-56 pb-1 object-contain rounded-full'
 <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
     <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
         <MagnifyingGlassIcon className='h-6 w-6 text-gray-400'/>
-        <input className='flex-1 outline-none p-2' type='text' placeholder='search'/>
+        <input  
+        type='text' 
+        placeholder='search' 
+        value={searchString} 
+        onChange={(e)=>setSearchString(e.target.value)}
+        className='flex-1 outline-none p-2'
+        
+        />
         <button type="submit" hidden>Search</button>
     </form>
 
@@ -37,10 +70,17 @@ className='w-44 md:w-56 pb-1 object-contain rounded-full'
 
 </div>
 <div className='flex items-center justify-center px-5 py-2 md:py-5'>
-    <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1] '>
-        <UserCircleIcon className='inline-block h-10 w-10 text-[#0055d1] mr-1'/>
+    <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]' >
+        <UserCircleIcon
+         className={`inline-block h-10 w-10 text-[#0055d1] mr-1 ${
+            loading && "animate-spin"
+         }`}
+         />
+         {suggestion&& !loading
+         ? suggestion
+        : "gpt is summarizing your day"}
     
-    gpt is summarizing your day
+    
     
     </p>
 </div>
---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,37 @@
+import openai from "@/openai";
+import {NextResponse} from "next/server";
+
+
+export async function POST (request:Request) {
+
+const {todos}=await request.json();
+console.log(todos);
+
+
+const response = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo",
+    temperature:0.8,
+    n:1,
+    stream:false,
+    messages:[
+        {
+            role:"system",
+            content:`When responding, welcome the user as Zsengeborso and say welcome to the agy nelkuliek  todo appjan!
+            limit the response to 200 characters`,
+        },
+        {
+            role:"user",
+            content:`hi there provide a summary of the following todos.count how many todos are in each category such as to do, in progress, and done then tell the user to have a productive day and make a simple joke regarding an office! here is the data: ${JSON.stringify(todos)}`,
+        }
+    ]
+
+});
+const {data} = response;
+
+
+console.log("DATA IS:",data);
+console.log(data.choices[0].message);
+
+return NextResponse.json(data.choices[0].message);
+
+}
